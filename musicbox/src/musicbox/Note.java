2e917928b10201090ca971aabc825b7fff5d93d6
Michael Strasser e1208048 <e1208048@student.tuwien.ac.at>
2015-05-23 19:21:00 +0200

    Added enum, defining valid note names

---
+++ musicbox/src/musicbox/Note.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musicbox;
+
+/**
+ * Defines valid note names.
+ * 
+ * @author Michael Strasser
+ */
+public enum Note {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G
+}
