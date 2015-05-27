65d0686b90a39b6e765e710d99fce81c9d0ff309
Michael Strasser <mst1409@gmx.at>
2015-05-27 16:29:50 +0200

    Removed bool for sharp notes from getNote(..)

--- musicbox/src/musicbox/NoteHelper.java
+++ musicbox/src/musicbox/NoteHelper.java
@@ -6,57 +6,9 @@
 package musicbox;
 
 /**
- * @TODO rewrite getNote() to work with sharp notes and without boolean
  * @author Michael Strasser
  */
 public class NoteHelper {
-    /**
-     * Calculates the midi number of a given note.
-     * @param note A valid note from the enum Note
-     * @param octave The octave of the note (0-8)
-     * @param sharp If true, the given note is sharp (e.g. "A#", "C#", etc.)
-     * If, for example, E is supplied as a note and sharp is true, the resulting
-     * note will be F and no error will occur.
-     * @return The midi note number (21-108)
-     * @throws Exception If an invalid note or octave is given.
-     */
-    public static int getNote( Note note, int octave, boolean sharp ) throws Exception {
-        int midi = -1;
-        
-        switch( note ) {
-            case A:
-                midi = 21;
-                break;
-            case B:
-                midi = 23;
-                break;
-            case C:
-                midi = 24;
-                break;
-            case D:
-                midi = 26;
-                break;
-            case E:
-                midi = 28;
-                break;
-            case F:
-                midi = 29;
-                break;
-            case G:
-                midi = 31;
-                break;
-            default:
-                throw new Exception("Invalid note given.");
-        }
-        
-        if( sharp ) midi++;
-        
-        if( octave >= 0 && octave <= 8 ) midi += 12*octave;
-        else throw new Exception("Invalid octave given.");
-        
-        return midi;
-    }
-    
     /**
      * Calculates the midi number of a given note.
      * @param note A valid note from the enum Note
@@ -65,7 +17,53 @@ public class NoteHelper {
      * @throws Exception If an invalid note or octave is given.
      */
     public static int getNote( Note note, int octave ) throws Exception {
-        return NoteHelper.getNote(note, octave, false);
+        int midi = -1;
+        
+        switch( note ) {
+            case A:
+                midi = 21;
+                break;
+            case Ais:
+                midi = 22;
+                break;
+            case B:
+                midi = 23;
+                break;
+            case C:
+                midi = 24;
+                break;
+            case Cis:
+                midi = 25;
+                break;
+            case D:
+                midi = 26;
+                break;
+            case Dis:
+                midi = 27;
+                break;
+            case E:
+                midi = 28;
+                break;
+            case F:
+                midi = 29;
+                break;
+            case Fis:
+                midi = 30;
+                break;
+            case G:
+                midi = 31;
+                break;
+            case Gis:
+                midi = 32;
+                break;
+            default:
+                throw new Exception("Invalid note given.");
+        }
+        
+        if( octave >= 0 && octave <= 8 ) midi += 12*octave;
+        else throw new Exception("Invalid octave given.");
+        
+        return midi;
     }
 
     /**
