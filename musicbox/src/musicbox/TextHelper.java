ae6dd78ffdf78aaa546bc6b5fe5229df023f62c5
Michael Strasser e1208048 <e1208048@student.tuwien.ac.at>
2015-05-27 16:21:36 +0200

    Helper class for handling the text input

---
+++ musicbox/src/musicbox/TextHelper.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musicbox;
+
+/**
+ *
+ * @author Michael Strasser
+ */
+public class TextHelper {
+    private String input;
+    
+    public void setInput( String input ) {
+        this.input = input;
+    }
+    
+    public String getInput() {
+        return this.input;
+    }
+}
