383fb1ce425245f803d6eded1928006091f29bfe
Michael Strasser e1208048 <e1208048@student.tuwien.ac.at>
2015-05-27 16:20:32 +0200

    Added enum definition of valid tone steps

---
+++ musicbox/src/musicbox/ToneStep.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musicbox;
+
+/**
+ * Definition of valid tone steps.
+ * W - Whole note step (e.g. from C to D).
+ * H - Half note step (e.g. from C to C#).
+ * WH - Whole and a half note step (e.g. from C to D#).
+ * 
+ * @author Michael Strasser
+ */
+public enum ToneStep {
+    W,
+    H,
+    WH
+}
